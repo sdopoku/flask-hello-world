255fff91c9b3caadd30d8730656e04aaf2a60373
David S Opoku <david@adoorelabs.com>
2015-09-16 16:53:35 +0000

    Add routes for integer, float and path values.

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ from flask import Flask
 app = Flask(__name__)
 
 # dynamic route
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
 @app.route("/test/<search_query>")
 def search(search_query):
     return search_query
@@ -21,4 +36,4 @@ def hello_world():
 
 # start the development server using the run() method
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
